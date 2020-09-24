d53348bed8980441597f63cb9931be69d385104f
federico <federico.buonco@gmail.com>
2020-09-24 18:47:05 +0200

    Trying to make connect_server raii

--- client/includes/connect_server/connect_server.h
+++ client/includes/connect_server/connect_server.h
@@ -4,11 +4,18 @@
 
 #ifndef CLIENT_CONNECT_SERVER_H
 #define CLIENT_CONNECT_SERVER_H
+#include <boost/asio.hpp>
 
 /// ConnectServer - establish a connection and auth
 class ConnectServer {
+private:
+    boost::asio::ip::tcp::socket sock_;
+    boost::asio::io_service ios_;
+    boost::asio::ip::tcp::endpoint ep_;
+
 public:
-    ConnectServer();
+    ConnectServer(const std::string& raw_ip_add,unsigned short port);
+   ~ConnectServer();
 };
 
 
