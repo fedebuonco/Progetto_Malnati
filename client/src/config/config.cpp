505aa32557dd07138aa4d55e77c47ffa4a72bcd2
fedeb <federico.buonco@gmail.com>
2020-11-22 16:10:49 +0100

    Added Dockerfile

--- client/src/config/config.cpp
+++ client/src/config/config.cpp
@@ -5,11 +5,11 @@
 #include <utilities.h>
 #include <filesystem>
 
+#include "cryptopp/cryptlib.h"
+#include <cryptopp/sha.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/hex.h>
 
-#include <sha.h>
-#include <filters.h>
-#include <hex.h>
-#include "cryptlib.h"
 
 
 bool DEBUG=false;
