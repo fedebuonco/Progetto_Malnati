3189ce538152543ed7ab0fe6faf5cba5a884d095
fedeb <federico.buonco@gmail.com>
2020-11-23 22:08:00 +0100

    Added Watcher using panoptes

--- client/includes/watcher/watcher.h
+++ client/includes/watcher/watcher.h
@@ -6,9 +6,35 @@
 #define CLIENT_WATCHER_H
 
 
-class watcher {
+#include <filesystem>
+#include <vector>
+#include <pfw/FileSystemWatcher.h>
+#include <pfw/NativeInterface.h>
+#include <bitset>
 
-};
+class Watcher{
+    public:
+        Watcher(std::filesystem::path path)
+        {
+            _watcher = std::make_unique<pfw::FileSystemWatcher>(
+                    path, std::chrono::milliseconds(1),
+                    std::bind(&Watcher::listenerFunction, this,
+                              std::placeholders::_1));
+        }
+        void listenerFunction(std::vector<pfw::EventPtr> events)
+        {
+            for (const auto &event : events) {
+                std::bitset<16> typeBits(event->type);
+                std::cout << event->relativePath << " with the type: " << typeBits
+                          << std::endl;
+            }
+        }
+
+    private:
+        std::unique_ptr<pfw::FileSystemWatcher> _watcher;
+    };
+
+
 
 
 #endif //CLIENT_WATCHER_H
