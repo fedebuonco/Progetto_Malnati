959446c138bb3326ae12bc4b41ff970db61fb726
Marco <marco.g1997@gmail.com>
2021-01-01 13:21:54 +0100

    Minor fix
    - document better main
    - starts to handling error code

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -2,15 +2,14 @@
 #include <iostream>
 #include <config.h>
 #include <client.h>
-#include <boost/property_tree/ptree.hpp>
-#include <watcher.h>
 #include <filesystem>
 
 int main(int argc, char *argv[]) {
-     /**
-     * CONFIGURATION PHASE
-     * Program reads and writes inside config the option with which the program was run
-     */
+    /**
+    * CONFIGURATION AND VALIDATION PHASE
+    * Program reads from the command line the new configuration options and writes it inside config.json
+    * Program validates the configuration parameters.
+    */
 
      //Before starting the configuration, we check if the config structure (file and folder) are correct
      if(!Config::get_Instance()->IsConfigStructureCorrect()){
@@ -19,39 +18,36 @@ int main(int argc, char *argv[]) {
          Config::get_Instance()->SetDefaultConfig();
      }
 
-    //Check if there are some arguments
-    if (argc > 1) {
 
-        //The user launch the program with configuration property
+    if (argc > 1) {
+        //The user provides some configuration arguments from the command line
         try {
+            //It tries to apply these new configuration options to the config_file
             Config::get_Instance()->SetConfig(argc, argv);
         }
         catch (std::exception& e){
-            //TODO: Controllare se tutto ok
+            //TODO: For me exit_failure is ok, we don't need an error code
             std::cerr << e.what() << std::endl;
-            std::exit(1);
+            std::exit(EXIT_FAILURE);
         }
-
-
     }
 
     //Shows the configuration with which the program is launched
     Config::get_Instance()->PrintConfiguration();
 
-    /**
-     * VALIDATION PHASE
-     * Program checks the validity of the config options
-     */
-
     //Check if the backup folder inside config.JSON exists
     if( !std::filesystem::exists(Config::get_Instance()->ReadProperty("path")) ){
         std::cerr << "Backup Folder doesn't exist. Check if you write it correctly and run the program again." << std::endl;
-        return 1;
+        return EXIT_FAILURE;
     }
+
     RawEndpoint raw_endpoint = Config::get_Instance()->ReadRawEndpoint();
+
     /**
+     * STARTUP
      * The main builds the client that will contain the main logic of the app.
      */
+
     // Building the Client
     // TODO These could also be a thread?
     std::filesystem::path path = std::filesystem::path(Config::get_Instance()->ReadProperty("path"));
@@ -59,5 +55,7 @@ int main(int argc, char *argv[]) {
 
     //TODO Change this leaving the main in the background.
     std::cin.ignore();
+
+    //TODO We need something to close the client once is running?
 }
 
