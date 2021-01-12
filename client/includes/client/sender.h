97ef13089534410289f60c93cc76b211f7c5ab56
Federica <federica.buccolini@hotmail.it>
2021-01-12 16:35:37 +0100

    TODO: finish threadpool

--- client/includes/client/sender.h
+++ client/includes/client/sender.h
@@ -13,23 +13,32 @@
 #include <queue>
 #include "file_sipper.h"
 #include <condition_variable>
+
+#define N 10
+
+
+
 /// This is the shared queue. Each FileSipper is demanded to send a file. The Sender decides who starts sending
 class Shared_Queue{
+
+public:
+
     std::queue<FileSipper> queue;
     std::mutex m;
 
-public:
     int size(){
         std::lock_guard<std::mutex> l(m);
         return queue.size();
     }
 
+    //Extract next FileSipper to start. Not remove from the queue!
     const FileSipper& extract(){
         std::lock_guard<std::mutex> l(m);
         const FileSipper& s =  queue.front();
-        queue.pop();
         return s;
     }
+
+
 
     void remove_end(){
         std::lock_guard<std::mutex> l(m);
@@ -41,17 +50,25 @@ public:
         queue.push(s);
     }
 
+
+
 };
 
 /// Takes files from the queue and gives them to FileSippers, in order to be sent
 class Sender {
+
+    //TODO: is it ok threads here??
+    std::vector<std::thread> threads[N];
 
     int count = 0;
     std::condition_variable cv;
 
     std::shared_ptr<Shared_Queue> shared_queue;
 public:
-    Sender( std::shared_ptr<Shared_Queue> queue): shared_queue(queue){};
+    Sender( std::shared_ptr<Shared_Queue> queue): shared_queue(queue){
+        //TODO: start threadpool
+        //start thread-pool
+    };
     void insert(const FileSipper& s);
     void Sender_Action();
 
--- client/src/client/sender.cpp
+++ client/src/client/sender.cpp
@@ -4,3 +4,31 @@
 void Sender::insert(const FileSipper& s){
     shared_queue.get()->add(s);
 }
+
+void Sender::Sender_Action(){
+
+    //TODO: is a good approach having here the working threadpool?
+
+    while(1){
+         if(count > N){
+            std::unique_lock<std::mutex> l(shared_queue->m);
+            cv.wait(l, [this](){ return count > N; } );
+         }
+
+        if(shared_queue->size() > 0){
+
+            threads->emplace_back( [this] () {
+                    //shared_queue->extract().Start(); //Start File Sipper
+
+            });
+
+
+
+
+        }
+
+
+
+    }
+
+}
