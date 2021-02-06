1824edfa6ffae3774433a6e134151aa104397247
Marco <marco.g1997@gmail.com>
2021-02-06 17:31:02 +0100

    Fix type

--- client/includes/transfer/control_message.h
+++ client/includes/transfer/control_message.h
@@ -8,26 +8,15 @@
 /// It has various attributes and a "to_json" method that will generate a result that will be sent to the server
 class ControlMessage {
 
-<<<<<<< Updated upstream
     /// Type of the message, in the server we switch actions depending on this type.
     boost::property_tree::ptree ptree_mess;
 
 public:
-    int type_;  //TODO: Make it private and create a method to access it @marco
+    int type_;
 
     explicit ControlMessage(int tp);
     explicit ControlMessage(const std::string& json_code);
 
-=======
-
-    boost::property_tree::ptree ptree_mess;
-
-public:
-    /// Type of the message, in the server we switch actions depending on this type.
-    int type_;
-    ControlMessage(int tp);
-    ControlMessage(const std::string& json_code);
->>>>>>> Stashed changes
     std::string ToJSON();
 
     void AddElement(const std::string& element,const std::string& value);
