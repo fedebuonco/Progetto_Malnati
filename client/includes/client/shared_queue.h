9f9a3255989d6b90e2c2d176edabedfdc2dfeced
Federica <federica.buccolini@hotmail.it>
2021-03-02 11:51:15 +0100

    varisbles

--- client/includes/client/shared_queue.h
+++ client/includes/client/shared_queue.h
@@ -6,17 +6,17 @@
 #define MAX_CONCURRENT_ACTIVE_FS 5
 /// This is the shared queue. Each FileSipper is demanded to send a file. The Sender decides who starts sending
 class SharedQueue{
-    //List of all shared_ptr to FileSippers
+    ///List of all shared_ptr to FileSippers
     std::list<std::shared_ptr<FileSipper>> fs_list;
-    //Protect shared list (fs_list) from being simultaneously accessed by multiple threads.
+    ///Protect shared list (fs_list) from being simultaneously accessed by multiple threads.
     std::mutex m;
-    //Condition Variable in order to block other threads.
+    ///Condition Variable in order to block other threads.
     std::condition_variable cv;
-    //Active fileSipper counter (fileSipper assigned to a thread of the pool)
+    ///Active fileSipper counter (fileSipper assigned to a thread of the pool)
     std::atomic<int> active_fs = 0;
-    //Shutdown flag
+    ///Shutdown flag
     std::atomic<bool> flag = true;
-    //Singletone pointer
+    ///Singletone pointer
     static SharedQueue* m_SharedQueue;
     SharedQueue()= default;
 
